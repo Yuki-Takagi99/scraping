47539824105b13b1db70c03cda330b6843adae4f
yuki takagi <yuki99takagi@gmail.com>
2020-08-07 12:00:53 +0900

    Add : Scraping.rbを作成

---
+++ scraping.rb
@@ -0,0 +1,16 @@
+require 'mechanize'
+
+def scrape
+  agent = Mechanize.new
+  page = agent.get("https://tabelog.com/soba/tokyo/rank/")
+  for i in 1..10
+    element = page.search("#container > div.rstlist-contents.clearfix > div > div.flexible-rstlst-main > ul > li:nth-child(#{i}) > div.list-rst__body > div.list-rst__contents > div > div.list-rst__rst-name-wrap > div.list-rst__rst-name > a")
+    store_name = element.inner_text
+    puts store_name
+    restaurant = Restaurant.new
+    restaurant.name = store_name
+    restaurant.save
+  end
+end
+
+scrape
